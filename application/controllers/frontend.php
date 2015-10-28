30899abad303fb5d25924debbe6801bf6cb3e10d
Miftah Aris Setiawan <miftahariss15@gmail.com>
2015-10-28 23:52:14 +0700

    intial commit

--- application/controllers/frontend.php
+++ application/controllers/frontend.php
@@ -48,8 +48,46 @@ class Frontend extends CI_Controller {
     public function update(){
         $data['base'] = 'Update';
 
-        $data['updates'] = $this->m_frontend->get_updates();
-        //var_dump($data['updates']);exit;
+        $limit = 6;
+
+        $this->db->where_in('status', array('1'));
+        $this->db->order_by("id", "desc");
+        $this->db->limit($limit, $this->uri->segment(2));
+        $photo_query = $this->db->get('update');
+        $data['updates'] = $photo_query->result_array();
+
+        $this->db->where_in('status', array('1'));
+        $query_total = $this->db->get('update');
+        $data['total'] = $query_total->num_rows();
+
+        $this->load->library('pagination');
+        $config['base_url'] = site_url('update');
+        $config['total_rows'] = $data['total'];
+        $config['per_page'] = $limit;
+        $config['uri_segment'] = 2;
+        $config['num_links'] = 2;
+        $config['prev_link'] = '&laquo;';
+        // $config['prev_tag_open'] = '<li><span><span aria-hidden="true">';
+        // $config['prev_tag_close'] = '</span></span></li>';
+        $config['next_link'] = '»';
+        // $config['next_tag_open'] = '<li><span aria-hidden="true">';
+        // $config['next_tag_close'] = '</span></li>';
+        $config['last_link'] = '';
+        $config['last_tag_open'] = '';
+        $config['last_tag_close'] = '';
+        $config['first_link'] = '';
+        $config['first_tag_open'] = '';
+        $config['first_tag_close'] = '';
+        // $config['num_tag_open'] = '<li><span>';
+        // $config['num_tag_close'] = '</span></li>';
+        $config['cur_tag_open'] = '<span class="page active">';
+        $config['cur_tag_close'] = '</span>';
+        $config['full_tag_open'] = '<div class="pagination">';
+        $config['full_tag_close'] = '</div>';
+        $config['anchor_class'] = 'class="page"';
+
+        $this->pagination->initialize($config);
+        $data['page_links'] = $this->pagination->create_links();
 
         $data['mainpage'] = 'frontend/update';
         $this->load->view('frontend/templates', $data);
@@ -57,6 +95,12 @@ class Frontend extends CI_Controller {
 
     public function detail(){
         $data['base'] = 'Update';
+
+        $permalink = $this->uri->segment(2);
+
+        $data['detail'] = $this->m_frontend->get_detail_updates($permalink);
+        //var_dump($data['detail'][0]->title);exit;
+        if($data['detail']==FALSE){redirect(show_404());die;}
 
         $data['mainpage'] = 'frontend/detail';
         $this->load->view('frontend/templates', $data);
