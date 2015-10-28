687ff4da72783858d81130f3fea24430df0c357f
Miftah Aris <miftahariss15@gmail.com>
2015-10-28 18:40:19 +0700

    intial commit

--- application/views/frontend/update.php
+++ application/views/frontend/update.php
@@ -1,5 +1,17 @@
 <div class="ilus"><img src="<?php echo base_url(); ?>asset/images/update_ilus.jpg"></div>
 	<div class="container_isi">
+        <?php if(isset($updates)): ?>
+          <?php foreach($updates as $value): ?>
+            <div class="list_art">
+                 <ul>
+                    <li class="list_poto"><img src="<?php echo base_url(); ?>asset/images/potho2.jpg" alt="" height="180px" width="300px"></li>
+                    <li class="list_judul"><a href="<?php echo base_url(); ?>update/<?php echo $value->permalink; ?>"><?php echo $value->title; ?></a></li>
+                    <li class="list_time">Sabtu, 19 April 2015  l   07.00 wib - 18.00 wib</li>
+                    <span><?php echo $value->short_desc; ?></span>
+                </ul>
+            </div>
+          <?php endforeach; ?>
+        <?php endif; ?>
     	<div class="list_art">
              <ul>
                 <li class="list_poto"><img src="<?php echo base_url(); ?>asset/images/potho1.jpg" alt="" height="180px" width="300px"></li>
@@ -49,8 +61,14 @@
           	</ul>
         </div><br class="clearfloat" />
         <div class="container">
-         <div class="pagination">
-			<a href="#" class="page">first</a><a href="#" class="page">2</a><a href="#" class="page">3</a><span class="page active">4</span><a href="#" class="page">5</a><a href="#" class="page">6</a><a href="#" class="page">last</a>
-	</div>
+        <div class="pagination">
+      			<a href="#" class="page">first</a>
+            <a href="#" class="page">2</a>
+            <a href="#" class="page">3</a>
+            <span class="page active">4</span>
+            <a href="#" class="page">5</a>
+            <a href="#" class="page">6</a>
+            <a href="#" class="page">last</a>
+      	</div>
     </div>
   </div>
