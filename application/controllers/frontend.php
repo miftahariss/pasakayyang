f3b401b2cc9d5a8cdb66086b89958847f4dd4c41
Miftah Aris <miftahariss15@gmail.com>
2015-10-29 11:01:53 +0700

    intial commit

--- application/controllers/frontend.php
+++ application/controllers/frontend.php
@@ -41,6 +41,47 @@ class Frontend extends CI_Controller {
     public function galeri(){
         $data['base'] = 'Galeri';
 
+        $limit = 12;
+
+        $this->db->where_in('status', array('1'));
+        $this->db->order_by("id", "desc");
+        $this->db->limit($limit, $this->uri->segment(2));
+        $photo_query = $this->db->get('galeri');
+        $data['galeri'] = $photo_query->result_array();
+
+        $this->db->where_in('status', array('1'));
+        $query_total = $this->db->get('galeri');
+        $data['total'] = $query_total->num_rows();
+
+        $this->load->library('pagination');
+        $config['base_url'] = site_url('galeri');
+        $config['total_rows'] = $data['total'];
+        $config['per_page'] = $limit;
+        $config['uri_segment'] = 2;
+        $config['num_links'] = 2;
+        $config['prev_link'] = '&laquo;';
+        // $config['prev_tag_open'] = '<li><span><span aria-hidden="true">';
+        // $config['prev_tag_close'] = '</span></span></li>';
+        $config['next_link'] = '»';
+        // $config['next_tag_open'] = '<li><span aria-hidden="true">';
+        // $config['next_tag_close'] = '</span></li>';
+        $config['last_link'] = 'last';
+        $config['last_tag_open'] = '';
+        $config['last_tag_close'] = '';
+        $config['first_link'] = 'first';
+        $config['first_tag_open'] = '';
+        $config['first_tag_close'] = '';
+        // $config['num_tag_open'] = '<li><span>';
+        // $config['num_tag_close'] = '</span></li>';
+        $config['cur_tag_open'] = '<span class="page active">';
+        $config['cur_tag_close'] = '</span>';
+        $config['full_tag_open'] = '<div class="pagination">';
+        $config['full_tag_close'] = '</div>';
+        $config['anchor_class'] = 'class="page"';
+
+        $this->pagination->initialize($config);
+        $data['page_links'] = $this->pagination->create_links();
+
         $data['mainpage'] = 'frontend/galeri';
         $this->load->view('frontend/templates', $data);
     }
@@ -72,10 +113,10 @@ class Frontend extends CI_Controller {
         $config['next_link'] = '»';
         // $config['next_tag_open'] = '<li><span aria-hidden="true">';
         // $config['next_tag_close'] = '</span></li>';
-        $config['last_link'] = '';
+        $config['last_link'] = 'last';
         $config['last_tag_open'] = '';
         $config['last_tag_close'] = '';
-        $config['first_link'] = '';
+        $config['first_link'] = 'first';
         $config['first_tag_open'] = '';
         $config['first_tag_close'] = '';
         // $config['num_tag_open'] = '<li><span>';
