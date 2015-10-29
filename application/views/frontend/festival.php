6e12ed1f01dc57b5fb62abfb3e8d8386d46e51a8
Miftah Aris <miftahariss15@gmail.com>
2015-10-29 17:10:06 +0700

    intial commit

--- application/views/frontend/festival.php
+++ application/views/frontend/festival.php
@@ -3,7 +3,7 @@
     	<div class="list_art">
              <ul>
                 <li class="list_poto"><img src="<?php echo base_url(); ?>asset/images/potho1.jpg" alt="" height="180px" width="300px"></li>
-                <li class="list_judul"><a href="#">Pembukaan Festifal Bajo Pasakayyang 2015</a></li>
+                <li class="list_judul">Pembukaan Festifal Bajo Pasakayyang 2015</li>
                 <li class="list_time">Sabtu, 19 April 2015  l   07.00 wib - 18.00 wib</li>
                 <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit</span>
           	</ul>
@@ -11,7 +11,7 @@
         <div class="list_art">
              <ul>
                 <li class="list_poto"><img src="<?php echo base_url(); ?>asset/images/potho2.jpg" alt="" height="180px" width="300px"></li>
-                <li class="list_judul"><a href="#">Pentas Budaya dan Kirab Seni bajo</a></li>
+                <li class="list_judul">Pentas Budaya dan Kirab Seni bajo</li>
                 <li class="list_time">Sabtu, 19 April 2015  l   07.00 wib - 18.00 wib</li>
                 <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit</span>
           	</ul>
@@ -19,7 +19,7 @@
         <div class="list_art">
              <ul>
                 <li class="list_poto"><img src="<?php echo base_url(); ?>asset/images/potho3.jpg" alt="" height="180px" width="300px"></li>
-                <li class="list_judul"><a href="#">Siapa sangka jika dia seorang lelaki</a></li>
+                <li class="list_judul">Siapa sangka jika dia seorang lelaki</li>
                 <li class="list_time">Sabtu, 19 April 2015  l   07.00 wib - 18.00 wib</li>
                 <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit</span>
           	</ul>
@@ -27,7 +27,7 @@
         <div class="list_art">
              <ul>
                 <li class="list_poto"><img src="<?php echo base_url(); ?>asset/images/potho1.jpg" alt="" height="180px" width="300px"></li>
-                <li class="list_judul"><a href="#">Siapa sangka jika dia seorang lelaki</a></li>
+                <li class="list_judul">Siapa sangka jika dia seorang lelaki</li>
                 <li class="list_time">Sabtu, 19 April 2015  l   07.00 wib - 18.00 wib</li>
                 <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit</span>
           	</ul>
@@ -35,7 +35,7 @@
         <div class="list_art">
              <ul>
                 <li class="list_poto"><img src="<?php echo base_url(); ?>asset/images/potho2.jpg" alt="" height="180px" width="300px"></li>
-                <li class="list_judul"><a href="#">Siapa sangka jika dia seorang lelaki</a></li>
+                <li class="list_judul">Siapa sangka jika dia seorang lelaki</li>
                 <li class="list_time">Sabtu, 19 April 2015  l   07.00 wib - 18.00 wib</li>
                 <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit</span>
           	</ul>
@@ -43,7 +43,7 @@
         <div class="list_art" style="margin-bottom:30px">
              <ul>
                 <li class="list_poto"><img src="<?php echo base_url(); ?>asset/images/potho3.jpg" alt="" height="180px" width="300px"></li>
-                <li class="list_judul"><a href="#">Siapa sangka jika dia seorang lelaki</a></li>
+                <li class="list_judul">Siapa sangka jika dia seorang lelaki</li>
                 <li class="list_time">Sabtu, 19 April 2015  l   07.00 wib - 18.00 wib</li>
                 <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit</span>
           	</ul>
