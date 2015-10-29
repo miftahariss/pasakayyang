022fb8ec06aaebd7c510b5c48115a01743d16942
Miftah Aris <miftahariss15@gmail.com>
2015-10-29 11:02:38 +0700

    intial commit

--- application/views/frontend/galeri.php
+++ application/views/frontend/galeri.php
@@ -1,115 +1,31 @@
 <div class="container" style="border-bottom: 1px solid rgba(136,132,133,1.00)"><img src="<?php echo base_url(); ?>asset/images/galeri_ico.png"></div>
 	<div class="container_isi">
     	<div class="boxes">
-        	<div class="box">
-            	<div class="box_photo">
-                <a href="<?php echo base_url(); ?>asset/images/potho01.png" rel="single"  class="fancybox-effects-a" title="Tablet PC dengan harga satu jutaan" style="margin:0 10px 0 0;">
-                <img src="<?php echo base_url(); ?>asset/images/potho01.png" alt="" width="300px" height="auto"></a></div>
-                <p>
-                	Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
-                </p>
-                
-            </div>
-            <div class="box">
-            	<div class="box_photo">
-                <a href="<?php echo base_url(); ?>asset/images/potho02.png" rel="single"  class="fancybox-effects-a" title="Tablet PC dengan harga satu jutaan" style="margin:0 10px 0 0;">
-                <img src="<?php echo base_url(); ?>asset/images/potho02.png" alt="" width="300px" height="auto"></a>
+            <?php if(isset($galeri)): ?>
+              <?php foreach($galeri as $value): ?>
+                <div class="box">
+                <div class="box_photo">
+                <a href="<?php echo base_url() ?>asset_admin/assets/uploads/cover/original/<?php echo $value['filename']; ?>" rel="single"  class="fancybox-effects-a" title="<?php echo $value['title']; ?>" style="margin:0 10px 0 0;">
+                <img src="<?php echo base_url() ?>asset_admin/assets/uploads/cover/original/<?php echo $value['filename']; ?>" alt="" width="300px" height="auto"></a>
                 </div>
                 <p>
-                	Lorem Ipsum has been the industry's standard dummy text ever since the 
+                    <?php echo $value['short_desc']; ?>
                 </p>
             </div>
-            <div class="box">
-            	<div class="box_photo">
-                <a href="<?php echo base_url(); ?>asset/images/potho03.png" rel="single"  class="fancybox-effects-a" title="Tablet PC dengan harga satu jutaan" style="margin:0 10px 0 0;">
-                <img src="<?php echo base_url(); ?>asset/images/potho03.png" alt="" width="300px" height="auto"></a></div>
-                <p>
-                	dummy text of the printing and typesetting industry standard dummy text ever since the 
-                </p>
-            </div>
-            <div class="box">
-            	<div class="box_photo">
-                <a href="<?php echo base_url(); ?>asset/images/potho04.png" rel="single"  class="fancybox-effects-a" title="Tablet PC dengan harga satu jutaan" style="margin:0 10px 0 0;">
-                <img src="<?php echo base_url(); ?>asset/images/potho04.png" alt="" width="300px" height="auto"></a></div>
-                <p>
-                	Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
-                </p>
-                
-            </div>
-            <div class="box">
-            	<div class="box_photo">
-                <a href="<?php echo base_url(); ?>asset/images/potho05.png" rel="single"  class="fancybox-effects-a" title="Tablet PC dengan harga satu jutaan" style="margin:0 10px 0 0;">
-                <img src="<?php echo base_url(); ?>asset/images/potho05.png" alt="" width="300px" height="auto"></a>
-                </div>
-                <p>
-                	Lorem Ipsum has been the industry's standard dummy text ever since the 
-                </p>
-            </div>
-            <div class="box">
-            	<div class="box_photo">
-                <a href="<?php echo base_url(); ?>asset/images/potho06.png" rel="single"  class="fancybox-effects-a" title="Tablet PC dengan harga satu jutaan" style="margin:0 10px 0 0;">
-                <img src="<?php echo base_url(); ?>asset/images/potho06.png" alt="" width="300px" height="auto"></a></div>
-                <p>
-                	dummy text of the printing and typesetting industry standard dummy text ever since the 
-                </p>
-            </div>
-            <div class="box">
-            	<div class="box_photo">
-                <a href="<?php echo base_url(); ?>asset/images/potho01.png" rel="single"  class="fancybox-effects-a" title="Tablet PC dengan harga satu jutaan" style="margin:0 10px 0 0;">
-                <img src="<?php echo base_url(); ?>asset/images/potho01.png" alt="" width="300px" height="auto"></a></div>
-                <p>
-                	Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
-                </p>
-                
-            </div>
-            <div class="box">
-            	<div class="box_photo">
-                <a href="<?php echo base_url(); ?>asset/images/potho02.png" rel="single"  class="fancybox-effects-a" title="Tablet PC dengan harga satu jutaan" style="margin:0 10px 0 0;">
-                <img src="<?php echo base_url(); ?>asset/images/potho02.png" alt="" width="300px" height="auto"></a>
-                </div>
-                <p>
-                	Lorem Ipsum has been the industry's standard dummy text ever since the 
-                </p>
-            </div>
-            <div class="box">
-            	<div class="box_photo">
-                <a href="<?php echo base_url(); ?>asset/images/potho03.png" rel="single"  class="fancybox-effects-a" title="Tablet PC dengan harga satu jutaan" style="margin:0 10px 0 0;">
-                <img src="<?php echo base_url(); ?>asset/images/potho03.png" alt="" width="300px" height="auto"></a></div>
-                <p>
-                	dummy text of the printing and typesetting industry standard dummy text ever since the 
-                </p>
-            </div>
-            <div class="box">
-            	<div class="box_photo">
-                <a href="<?php echo base_url(); ?>asset/images/potho04.png" rel="single"  class="fancybox-effects-a" title="Tablet PC dengan harga satu jutaan" style="margin:0 10px 0 0;">
-                <img src="<?php echo base_url(); ?>asset/images/potho04.png" alt="" width="300px" height="auto"></a></div>
-                <p>
-                	Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
-                </p>
-                
-            </div>
-            <div class="box">
-            	<div class="box_photo">
-                <a href="<?php echo base_url(); ?>asset/images/potho05.png" rel="single"  class="fancybox-effects-a" title="Tablet PC dengan harga satu jutaan" style="margin:0 10px 0 0;">
-                <img src="<?php echo base_url(); ?>asset/images/potho05.png" alt="" width="300px" height="auto"></a>
-                </div>
-                <p>
-                	Lorem Ipsum has been the industry's standard dummy text ever since the 
-                </p>
-            </div>
-            <div class="box">
-            	<div class="box_photo">
-                <a href="<?php echo base_url(); ?>asset/images/potho06.png" rel="single"  class="fancybox-effects-a" title="Tablet PC dengan harga satu jutaan" style="margin:0 10px 0 0;">
-                <img src="<?php echo base_url(); ?>asset/images/potho06.png" alt="" width="300px" height="auto"></a></div>
-                <p>
-                	dummy text of the printing and typesetting industry standard dummy text ever since the 
-                </p>
-            </div>
+              <?php endforeach; ?>
+            <?php endif; ?>
             <br class="clearfloat" />
             	<div class="container">
-         			<div class="pagination">
-			<a href="#" class="page">first</a><a href="#" class="page">2</a><a href="#" class="page">3</a><span class="page active">4</span><a href="#" class="page">5</a><a href="#" class="page">6</a><a href="#" class="page">last</a>
-					</div>
+                <?php echo $page_links; ?>
+     			<?php /* <div class="pagination">
+		          <a href="#" class="page">first</a>
+                  <a href="#" class="page">2</a>
+                  <a href="#" class="page">3</a>
+                  <span class="page active">4</span>
+                  <a href="#" class="page">5</a>
+                  <a href="#" class="page">6</a>
+                  <a href="#" class="page">last</a>
+				</div> */ ?>
     			</div>
                 </div>
         </div>
