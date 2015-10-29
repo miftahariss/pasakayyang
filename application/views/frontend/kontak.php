af46dffe29782cf40a76b22ad7eaeb51af06f866
Miftah Aris <miftahariss15@gmail.com>
2015-10-29 11:04:34 +0700

    intial commit

---
+++ application/views/frontend/kontak.php
@@ -0,0 +1,33 @@
+<div class="ilus" style="margin-bottom:20px"><img src="<?php echo base_url(); ?>asset/images/kontak_ilus.jpg">
+     </div>
+	<div class="container">
+
+<p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet, consectetur, adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+  </div>
+     <br class="clearfloat" />
+     <div class="container_full bg">
+    
+		<!-- end .slider -->
+        <!-- begin .boxes -->
+        
+       <div class="boxes">
+        	<div class="box" style="text-align: right; line-height:33px; margin-top:20px">
+            	<p style="font-size:18px; font-weight:bold">Contact Info</p>
+<p>Lorem ipsum dolor sit amet, consecteturpiscinger elit um dolor sit amet, consecteturpiscing.
+If you have any questions about customization of free themes, address 
+to guys from TemplateTuning. Note that it's a paid service</p>
+            </div>
+            <div class="box">
+            	<img src="<?php echo base_url(); ?>asset/images/kontak_ico.png" style="box-shadow:none">
+            </div>
+            <div class="box" style="text-align:left; line-height:33px; margin-top:20px">
+				<p style="font-size:18px; font-weight:bold">NAMA KONTAK</p>
+<p>Jl. Pangeran Diponegoro 9870 Teluk Jambe Morowali Selatan Indonesia Place, 
+Glasgow, DC 45 Fr 45. 
+Telephone		: +1 800 603 6035 
+FAX				: +1 800 889 9898 
+E-mail			: mail@demolink.org</p>
+            </div>
+            <br class="clearfloat" />
+        </div>
+  </div>
